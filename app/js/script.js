2653bac0d243c1c1f31fbb31d0cbb5cfe64a79a1
ChocoChipWookie <skabir@codeclubs.org>
2020-10-26 23:54:24 -0600

    working with 1 question toggled

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+var active = false;
+
+function toggleAnswer() {
+    var x = document.getElementById("a1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+
+    var y = document.getElementById("q1");
+    if (active) {
+        active = false;
+        y.style.fontWeight = "700";
+    } else {
+        active = true;
+        y.style.fontWeight = "300";
+    }
+}
